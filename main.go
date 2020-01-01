b15b5eeb31bd5926e4b48f28d9c95844547b0ba1
Colin Fletcher <colin.fletcher@fiserv.com>
2020-01-01 14:05:52 -0500

    Handle and Handle Func Endpoints

--- main.go
+++ main.go
@@ -2,20 +2,52 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"sync"
 )
 
-const port = ":8080"
+const (
+	port        = ":8080"
+	userAPIResp = `
+<p> Hello, World! 
+<p> Request received : %q endpoint
+<p> HTTP Method : %v
+<p> This is call %v to this API
+`
+	countAPIResp = `
+<p> Hello, World! 
+<p> Request received : %q endpoint
+<p> HTTP Method : %v
+<p> API Call Count: %v
+`
+)
 
-func main() {
-	http.HandleFunc("/users", userHandlerFunc)
-	log.Fatal(http.ListenAndServe(port, nil))
+var userCount int
+
+type countHandler struct {
+	mu sync.Mutex //guards nil
+	n  int
 }
 
 func userHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	fmt.Println("Request received : /users endpoint")
-	fmt.Fprintf(w, "Response logged for API HTTP Method '%v'", r.Method)
+	fmt.Fprintf(w, "Hello, World")
+	userCount++
+	out := fmt.Sprintf(userAPIResp, r.URL.Path, r.Method, userCount)
+	fmt.Fprintf(w, out)
+}
+
+func (h *countHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.n++
+	out := fmt.Sprintf(countAPIResp, r.URL.Path, r.Method, h.n)
+	fmt.Fprintf(w, out)
+	fmt.Fprintf(w, "count is %d\n", h.n)
+}
+
+func main() {
+	http.HandleFunc("/users/", userHandlerFunc)
+	http.Handle("/count", new(countHandler))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
